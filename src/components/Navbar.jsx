f128194509af8ad05a77c71aba8074e768f92cb7
janxllea <janrodriguez@calteks.com>
2023-10-31 15:01:31 -0500

    gtnw commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { BiMenuAltRight,  BiX} from "react-icons/bi";
+import Logo from "../assets/logo.png"
 
 const Navbar = () => {
-return (
-    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-        <h1 className='w-full text-3xl font-bold'>Cal Estate Holdings</h1>
-        <ul className='flex'>
-            <li className='p-4'>HOME</li>
-            <li className='p-4'>SERVICES</li>
-            <li className='p-4'>CONTACT</li>
-        </ul>
-    </div>
-)
-}
+  const [nav, setNav] = useState(false);
 
-export default Navbar
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  return (
+    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
+      <img className='w-[200px] ' src={Logo} alt="/" />
+      <ul className='hidden md:flex'>
+        <li className='p-4'>Home</li>
+        <li className='p-4'>Company</li>
+        <li className='p-4'>About</li>
+        <li className='p-4'>Contact</li>
+      </ul>
+      <div onClick={handleNav} className='block md:hidden'>
+          {nav ? <BiX size={20}/> : <BiMenuAltRight size={20} />}
+      </div>
+      <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+        <img className='w-[200px] p-2' src={Logo} alt="/" />
+          <li className='p-4 border-b border-gray-600'>Home</li>
+          <li className='p-4 border-b border-gray-600'>Company</li>
+          <li className='p-4 border-b border-gray-600'>Resources</li>
+          <li className='p-4 border-b border-gray-600'>About</li>
+          <li className='p-4'>Contact</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
